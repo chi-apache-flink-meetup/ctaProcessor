43b65c6d72c172651591127314dcd7f75a0a4f5c
Joe Olson <jolson7168@nododos.com>
2017-02-20 09:27:32 -0600

    initial add

---
+++ code/loaders/ctaLoader/src/main/scala/org/chiflink/ctaprocessor/loaders/ctaloader/LoadFixes.scala
@@ -0,0 +1,10 @@
+package org.chiflink.ctaprocessor.loaders.ctaloader
+
+/**
+  * Created by ubuntu on 2/20/17.
+  */
+object LoadFixes {
+  def main(args: Array[String]): Unit = {
+    println("Hello World!")
+  }
+}
